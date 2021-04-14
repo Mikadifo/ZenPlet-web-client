c6eb8a07430f2f1ff3050b339603e7a333b1d201
Michael <michaelpadillaheredia@hotmail.com>
2021-04-14 11:37:53 -0500

    default route link setted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zenplet-web-client';
+
+  constructor(private router: Router) {
+    router.navigate([
+      localStorage.getItem('owner') === null ? '/auth' : '/list/pets',
+    ]);
+  }
 }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logOut() {
+    localStorage.removeItem('owner');
+    this.router.navigate(['/auth']);
   }
-
 }
