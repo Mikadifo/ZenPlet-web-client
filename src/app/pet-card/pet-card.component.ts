e740c7f54709ffd0eaafda30bf982a4b36540219
Michael <michaelpadillaheredia@hotmail.com>
2021-04-15 16:47:20 -0500

    Fixed pet age format

--- src/app/pet-card/pet-card.component.ts
+++ src/app/pet-card/pet-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Owner } from '../model/owner/owner.model';
 import { Pets } from '../model/pets.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-pet-card',
@@ -21,13 +22,21 @@ export class PetCardComponent implements OnInit {
     petVaccines: [],
   };
 
-  petAge: number = 0;
+  petAge: string = '';
 
   constructor() {}
 
   ngOnInit(): void {
-    let petBirthdateAsDate = new Date(this.pet.petBirthdate);
-    let timeDiff = Math.abs(Date.now() - petBirthdateAsDate.getTime());
-    this.petAge = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+    let petBirthdate: any = moment(this.pet.petBirthdate, 'YYYY-MM-DD');
+    let now: any = moment();
+    let petYears = now.diff(petBirthdate, 'year');
+    petBirthdate.add(petYears, 'years');
+    let petMonths = now.diff(petBirthdate, 'month');
+    petBirthdate.add(petMonths, 'months');
+    let petDays = now.diff(petBirthdate, 'day');
+
+    this.petAge = `${petYears} year(s) ${petMonths} month(s) ${petDays} day(s)`;
+
+    console.log(this.petAge);
   }
 }
